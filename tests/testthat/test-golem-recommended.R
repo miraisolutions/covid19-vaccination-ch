fb2cc6e99f9a38f0604ed761d45f387b489421d9
GuidoMaggio <guido.maggio@mirai-solutions.ch>
2022-02-08 17:32:39 +0100

    Removed test

--- tests/testthat/test-golem-recommended.R
+++ tests/testthat/test-golem-recommended.R
@@ -18,9 +18,9 @@ test_that("app server", {
   }
 })
 
-# Configure this test to fit your need
-test_that(
-  "app launches",{
-    golem::expect_running(sleep = 5)
-  }
-)
+# # Configure this test to fit your need
+# test_that(
+#   "app launches",{
+#     golem::expect_running(sleep = 5)
+#   }
+# )
